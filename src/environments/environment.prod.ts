3bd897a537acff91985971430417b5ec34699c98
Natams7526 <developmentnm2022@gmail.com>
2023-12-28 11:58:05 -0500

    Change the url EB to gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 // config.ts
 export const environment = {
-  apiUrl: 'http://bk-lbda-env.eba-axtn3vdb.us-east-1.elasticbeanstalk.com',
+  apiUrl: 'https://g3znbplu75.execute-api.us-east-1.amazonaws.com/prod',
+  // apiUrl: 'http://bk-lbda-env.eba-axtn3vdb.us-east-1.elasticbeanstalk.com',
   port: '',
   path: {
     apointment: '/appointments',
